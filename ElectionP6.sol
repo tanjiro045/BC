d3cd05faebb036d4149afc56dd3e59c22c5c5d9a
tanjiro045 <136693903+tanjiro045@users.noreply.github.com>
2023-11-04 10:07:03 +0530

    Create ElectionP6.sol - P6

---
+++ ElectionP6.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract ElectionP6 {
+// Model a Candidate
+struct Candidate {
+uint id;
+string name;
+uint voteCount;
+}
+// Store accounts that have voted
+mapping(address => bool) public voters;
+// Store Candidates
+// Fetch Candidate
+mapping(uint => Candidate) public candidates;
+// Store Candidates Count
+uint public candidatesCount;
+// voted event
+event votedEvent(uint indexed _candidateId);
+constructor() {
+addCandidate("N MODI, BJP");
+addCandidate("A kejriwal, AAP");
+addCandidate("Rahul G, Congress");
+addCandidate("Nikhil, JDS");
+}
+function addCandidate(string memory _name) private {
+candidatesCount++;
+candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+}
+function vote(uint _candidateId) public {
+// require that they haven't voted before
+require(!voters[msg.sender], "You have already voted.");
+// require a valid candidate
+require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate ID.");
+// record that voter has voted
+voters[msg.sender] = true;
+// update candidate vote Count
+candidates[_candidateId].voteCount++;
+// trigger voted event emit
+emit votedEvent(_candidateId);
+}
+}
